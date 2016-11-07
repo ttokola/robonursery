d9f4fb0f5f7a48ec69d2bca53f275cde4be85788
Jussi Virtanen <virtajus@student.oulu.fi>
2016-11-07 17:14:14 +0200

    Fix ignore collision script to take multiple colliders between which collisions should be ignored

--- Assets/Scripts/IgnoreCollision.cs
+++ Assets/Scripts/IgnoreCollision.cs
@@ -1,26 +1,35 @@
 ﻿/*
-    Ignore collision between collider pairs
+    Ignore collision between sets of colliders
 */
 
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
+// http://answers.unity3d.com/questions/289692/serialize-nested-lists.html
 [System.Serializable]
-public struct ColliderPair
+public class ListWrapper
 {
-    public Collider coll1;
-    public Collider coll2;
+    public List<Collider> colliders;
 }
 
 public class IgnoreCollision : MonoBehaviour {
 
-    public ColliderPair[] colliderPairs;
+    public List<ListWrapper> ignoredColliderSets;
     
 	void Start ()
     {
-        foreach (ColliderPair cp in colliderPairs)
+        foreach (ListWrapper lw in ignoredColliderSets)
         {
-            Physics.IgnoreCollision(cp.coll1, cp.coll2);
+            // These loops just create all the possible combinations from the collider set
+            // A bit ugly but couldn't find native solution for list combinations
+            for (int i = 0; i < lw.colliders.Count; i++)
+            {
+                for (int j = i+1; j < lw.colliders.Count; j++)
+                {
+                    Physics.IgnoreCollision(lw.colliders[i], lw.colliders[j]);
+                }
+            }
         }
-	}
+    }
 }
