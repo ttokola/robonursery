1f7bf7e5a9e536bd5580496642f6898eed473281
nikolehto <niko.lehto@mbnet.fi>
2016-12-04 16:45:51 +0200

    First step to scenario control
    Scenario script creates instance of LilBot, activates picking, and sets
    target. I've been working on 'test_area' scene.

--- Assets/Scripts/Scenario/ScenarioScript.cs
+++ Assets/Scripts/Scenario/ScenarioScript.cs
@@ -4,11 +4,30 @@ using System.Collections;
 public class ScenarioScript : MonoBehaviour
 {
     public GameObject prefab;
+    public GameObject tar;
+    LilBotNamespace.PickupObject picking;
+    //public 
 
     void Start()
     {
         
-        Instantiate(prefab, new Vector3(0.0f, 3.0f, 0.0f), Quaternion.identity);
+        GameObject bot = (GameObject) Instantiate(prefab, new Vector3(0.0f, 0.5f, 0.0f), Quaternion.identity);
+
+        
+
+        picking = bot.GetComponent<LilBotNamespace.PickupObject>();
+        picking.tar = tar;
+
+        picking.enabled = true;
+
+
+
+        bool value = bot.GetComponent<Battery>().isActiveAndEnabled;
+
+        bool value2 = bot.GetComponent < LilBotNamespace.MovementControls>();
+
+        Debug.Log(value);
+        Debug.Log(value2);
     }
 
 // Update is called once per frame
