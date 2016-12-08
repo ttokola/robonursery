a287995e3fb1411cddd173e6caae5aa603636d65
Jussi Virtanen <virtajus@student.oulu.fi>
2016-12-08 03:51:14 +0200

    One way of sequencing

---
+++ Assets/Scripts/LilBot/SequenceExample.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using LilBotNamespace;
+
+namespace LilBotNamespace
+{
+
+public class SequenceExample : MonoBehaviour {
+    
+    private int state;
+    
+    public GameObject ball;
+    
+    public ArmControls armControls;
+    public MovementControls movementControls;
+    public PickupObject pickupRoutine;
+    
+    void Start ()
+    {
+        state = 0;
+    }
+	
+	void FixedUpdate ()
+    {
+        switch (state)
+        {
+        case 0:
+            armControls.SetStaticPosition("idle");
+            if (movementControls.DriveTo(Vector3.zero, true) == 1)
+            {
+                state = 1;
+            }
+            break;
+        case 1:
+            if (pickupRoutine.Execute(ball) == 0)
+            {
+                state = 2;
+            }
+            break;
+        case 2:
+            if (movementControls.DriveTo(Vector3.zero, true) == 1)
+            {
+                pickupRoutine.Drop();
+                state = 3;
+            }
+            break;
+        case 3:
+            armControls.SetStaticPosition("idle");
+            break;
+        }
+    }
+}
+
+} // End namespace
