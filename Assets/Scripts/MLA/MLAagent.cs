d87c60766062fd0e8e5f6c1af68afcaa2649cce1
osuvileh <otso.suvilehto@student.oulu.fi>
2017-11-13 22:06:07 +0200

    Initial tests on moving components

--- Assets/Scripts/MLA/MLAagent.cs
+++ Assets/Scripts/MLA/MLAagent.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using LilBotNamespace;
+
 public class MLAagent : Agent {
+
     
+
     public MovementControls Move;
     public BallJoint Arms;
+    public ExampleControl example;
 
     public float leftWheel;
     public float rightWheel;
@@ -25,6 +29,30 @@ public class MLAagent : Agent {
     public float rightEbowX;
     public float rightElbowY;
     public float rightElbowZ;
+    private Vector3 shoulderForce;
+
+    private Rigidbody exampleRB;
+    private Transform tf;
+
+    public void MoveArm(Transform obj, float force)
+    {
+
+        exampleRB.AddTorque(tf.up * force * 20f);
+    }
+
+    private void Start()
+    {
+        AgentProto AgentProto = this.gameObject.GetComponent<AgentProto>();
+        List<AgentParser.Component> components = AgentProto.GetComponents(); //All components of the robot
+        AgentProto.Component obj = AgentProto.GetComponentByName("LUpperArm"); //One AgentParser.Component
+        //AgentProto.Component obj = components[8];
+        exampleRB = obj.gameObject.GetComponent<Rigidbody>(); //AgentParser.Component.gameObject contains the UnityEngine.GameObject
+        tf = obj.gameObject.GetComponent<Transform>();
+        exampleRB.maxAngularVelocity = 100f;
+        Vector3 shoulderForce = new Vector3(leftShoulderX, leftShoulderY, leftShoulderZ);
+        
+
+    }
 
     public override List<float> CollectState()
 	{
@@ -58,10 +86,13 @@ public class MLAagent : Agent {
         rightEbowX = act[11];
         rightElbowY = act[12];
         rightElbowZ = act[13];
-        
+
         //move the wheels using controls from Movementcontrols script
-        Move.RotateWheel("left",act[0]);
-        Move.RotateWheel("right", act[1]);
+        //Move.RotateWheel("left",act[0]);
+        //Move.RotateWheel("right", act[1]);
+        
+        
+        MoveArm(tf, act[0]);
         reward =+ 1f;
         if(act[0] >9 || act[1] >9)
         {
