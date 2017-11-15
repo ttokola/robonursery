7881f036a9c355339afe4a21da4452525985f966
jpajukan <jpajukan@gmail.com>
2017-11-15 15:57:00 +0200

    Added some testing for new carrying script. Disable test run by disabling TriggerCarryScriptObject

---
+++ Assets/Scripts/testscript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscript : MonoBehaviour {
+
+    public GameObject targetobj;
+    public Transform targettransform;
+
+    public CarryObject carryObjScript;
+    
+
+	// Use this for initialization
+	void Start () {
+        carryObjScript.setTask(targetobj, targettransform, 0.0f, new Vector3(0,-1, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
