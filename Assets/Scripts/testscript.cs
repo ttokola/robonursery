4a2faa93addfdb0a188e6a76c0cff7975a92db24
jpajukan <jpajukan@gmail.com>
2017-11-21 21:31:16 +0200

    Modify test script for demonstrating hand waving

--- Assets/Scripts/testscript.cs
+++ Assets/Scripts/testscript.cs
@@ -8,11 +8,19 @@ public class testscript : MonoBehaviour {
     public Transform targettransform;
 
     public CarryObject carryObjScript;
+
+
+    public GameObject lilbot;
+    public GameObject nannybot;
     
 
 	// Use this for initialization
 	void Start () {
-        carryObjScript.setTask(targetobj, targettransform, 0.0f, new Vector3(0,-1, 0));
+        //carryObjScript.setTask(targetobj, targettransform, 0.0f, new Vector3(0,-1, 0));
+
+        SayHello sh = nannybot.GetComponent<SayHello>();
+
+        sh.setTask(lilbot, 3, true);
 	}
 	
 	// Update is called once per frame
