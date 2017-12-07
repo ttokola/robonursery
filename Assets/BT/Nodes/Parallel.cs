20bbbc9a281dd869243f4bafdd2249fbcf117b78
nykm <nykm@users.noreply.github.com>
2017-12-07 09:23:24 +0200

    Initial parallel BT node support

---
+++ Assets/BT/Nodes/Parallel.cs
@@ -0,0 +1,33 @@
+﻿namespace BT
+{
+    public class Parallel : Composite
+    {
+        protected override Status Update()
+        {
+            int running = 0;
+
+            foreach (var child in GetChildren())
+            {
+                Status status = child.Tick();
+
+                if (status == Status.Running)
+                {
+                    running++;
+                }
+                else if (status != Status.Success)
+                {
+                    return Status.Failure;
+                }
+            }
+
+            if (running > 0)
+            {
+                return Status.Running;
+            }
+            else
+            {
+                return Status.Success;
+            }
+        }
+    }
+}
