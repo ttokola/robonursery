e6721c0718475f0bdda46394002e9a5686745799
nykm <nykm@users.noreply.github.com>
2017-12-07 09:39:59 +0200

    Example controller for the experimental scenario system

---
+++ Assets/Scenarios/TestController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using BT;
+using Env;
+
+public class TestController : MonoBehaviour
+{
+    private Sequence schedule;
+
+    void Start()
+    {
+        schedule = new Sequence();
+
+        var scenarios = transform.Find("Scenarios").GetComponents<Scenario>();
+
+        foreach (var scenario in scenarios)
+        {
+            Node node = scenario.Create();
+            schedule.Add(new DebugAction(scenario.GetType().Name + " start"));
+            schedule.Add(node);
+            schedule.Add(new DebugAction(scenario.GetType().Name + " end"));
+        }
+    }
+
+    void Update()
+    {
+        if (schedule != null)
+        {
+            schedule.Tick();
+        }
+    }
+}
