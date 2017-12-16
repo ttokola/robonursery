f3b4d37d8d9d5b055daeba5b2c6be8a804ab010b
jpajukan <jpajukan@gmail.com>
2017-12-16 16:22:56 +0200

    Carryobject script code cleanup and commenting

--- Assets/Scripts/Bot/Routines/CarryObject.cs
+++ Assets/Scripts/Bot/Routines/CarryObject.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿/*
+ * Grab object and carry it to the defined position
+ * Set carrying task for robot by using setTask function
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using LilBotNamespace;
@@ -7,10 +12,14 @@ using LilBotNamespace;
 
 public class CarryObject : MonoBehaviour {
 
+    [Tooltip("Drag the Transform of left hand of robot here")]
     public Transform leftHand;
+    [Tooltip("Drag the Transform of right hand of robot here")]
     public Transform rightHand;
 
+    [Tooltip("Drag the ArmControls script attached to the robot here")]
     public ArmControls armControls;
+    [Tooltip("Drag the MovementControls script attached to the robot here")]
     public MovementControls movementControls;
 
     public float movementRange;
@@ -25,14 +34,16 @@ public class CarryObject : MonoBehaviour {
 
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    /* Task execution working as state machine
+     * If state is 0 then execution does not happen
+     */
+    {
         switch (state)
         {
             case 0:
                 break;
             case 1:
-                //jostain syystä en onnistu mitenkään laittamaan defaulttiparametrejä
                 if (movementControls.DriveTo(carryObject.transform, true) == 0)
                 {
                     state = 2;
@@ -72,15 +83,23 @@ public class CarryObject : MonoBehaviour {
 	}
 
 
-    public int setTask(GameObject co, Transform ct, float mr, Vector3 go){
+    public int setTask(GameObject co, Transform ct, float mr, Vector3 go)
+    /*
+     * Set carrying task for robot 
+     * 
+     * Set target object, target location where object is to be carried and offset vector for carrying object
+     */
+    {
         carryObject = co;
         carryTarget = ct;
         grabOffset = go;
+        movementRange = mr;
         state = 1;
         return 0;
     }
 
     private int setKinematic()
+    //Sets carried object (including child objects) to be kinematic
     {
         Component[] rbs;
         rbs = carryObject.GetComponentsInChildren(typeof(Rigidbody));
@@ -101,6 +120,7 @@ public class CarryObject : MonoBehaviour {
     }
 
     private int setNormal()
+    //Sets carried object (including child objects) to be normal
     {
         Component[] rbs;
         rbs = carryObject.GetComponentsInChildren(typeof(Rigidbody));
@@ -121,8 +141,25 @@ public class CarryObject : MonoBehaviour {
     }
 
     private int attachTargetToHand()
+    // When carrying object, this function is called to attach kinematic object to robot hands
     {
         carryObject.transform.position = leftHand.position + grabOffset;
         return 0;
+    }
+
+    public int getState()
+    {
+        return state;
+    }
+
+    public void cancelTask()
+    // Stop executing task
+    {
+        if (state != 0)
+        {
+            setNormal();
+        }
+        state = 0;
+
     }
 }
