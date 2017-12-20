b5a94f2ab477832bc390bba3cd116c1999517f83
nykm <nykm@users.noreply.github.com>
2017-12-21 01:21:18 +0200

    Add more explicit success and failure condition checking to parallel node

--- Assets/BT/Nodes/Composite.cs
+++ Assets/BT/Nodes/Composite.cs
@@ -5,6 +5,7 @@ namespace BT
     public abstract class Composite : Node
     {
         private readonly List<Node> children = new List<Node>();
+        public int ChildCount { get { return children.Count; } }
 
         public void Add(Node child)
         {
--- Assets/BT/Nodes/Parallel.cs
+++ Assets/BT/Nodes/Parallel.cs
@@ -2,31 +2,105 @@
 {
     public class Parallel : Composite
     {
+        private readonly ICondition successCondition;
+        private readonly ICondition failureCondition;
+
+        public Parallel() : this(Condition.All, Condition.One)
+        {
+        }
+
+        public Parallel(
+            ICondition successCondition,
+            ICondition failureCondition)
+        {
+            this.successCondition = successCondition;
+            this.failureCondition = failureCondition;
+        }
+
         protected override Status Update()
         {
-            int running = 0;
+            int successCount = 0;
+            int failureCount = 0;
 
             foreach (var child in GetChildren())
             {
                 Status status = child.Tick();
 
-                if (status == Status.Running)
+                if (status == Status.Success)
                 {
-                    running++;
+                    successCount++;
                 }
-                else if (status != Status.Success)
+                else if (status == Status.Failure)
                 {
-                    return Status.Failure;
+                    failureCount++;
                 }
+
+                // Question/improvement:
+                // Fail/success here instead at the end? Either way would be
+                // somewhat "correct" as tasks are executed in a parallel manner
+                // (well, not actually...).
             }
 
-            if (running > 0)
+            Status result = Check(successCount, failureCount);
+
+            Log.Verbose("successCount: {0}, failureCount: {1}, result: {2}",
+                successCount, failureCount, result);
+
+            return result;
+        }
+
+        private Status Check(int successCount, int failureCount)
+        {
+            if (IsFailure(failureCount))
             {
-                return Status.Running;
+                return Status.Failure;
+            }
+            else if (IsSuccess(successCount))
+            {
+                return Status.Success;
             }
             else
             {
-                return Status.Success;
+                return Status.Running;
+            }
+        }
+
+        private bool IsSuccess(int successCount)
+        {
+            return successCondition.Check(successCount, this);
+        }
+
+        private bool IsFailure(int failureCount)
+        {
+            return failureCondition.Check(failureCount, this);
+        }
+
+        public interface ICondition
+        {
+            bool Check(int count, Parallel node);
+        }
+
+        public class Condition : ICondition
+        {
+            public static readonly Condition One
+                = new Condition { Count = 1 };
+            public static readonly Condition All
+                = new Condition { Count = -1 };
+
+            public int Count { get; private set; }
+
+            public bool Check(int count, Parallel node)
+            {
+                int threshold = IsAllRequired()
+                    ? node.ChildCount /* All */
+                    : Count           /* Custom */;
+
+                return count >= threshold;
+            }
+
+            private bool IsAllRequired()
+            {
+                return Count == -1;
             }
         }
     }
