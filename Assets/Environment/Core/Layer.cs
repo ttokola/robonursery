23c245678c35480cd94501583903bf2c9b3ac7c8
nykm <nykm@users.noreply.github.com>
2017-12-21 02:19:14 +0200

    Initial supervisor component
    - Work in progress, TODO: optimization, error checking, examples
      and documentation
    - Layers for, e.g., debugging, scoring and environment actions

---
+++ Assets/Environment/Core/Layer.cs
@@ -0,0 +1,79 @@
+﻿using BT;
+using System;
+using System.Collections.Generic;
+
+namespace RobotNursery
+{
+    public class Layer
+    {
+        private List<Binding> bindings = new List<Binding>();
+
+        public void Reset()
+        {
+            bindings.ForEach(binding => binding.Reset());
+        }
+
+        public void Step()
+        {
+            bindings.ForEach(binding => binding.Step());
+        }
+
+        public void Bind(Node trigger, Node branch)
+        {
+            if (trigger == null)
+            {
+                throw new NullReferenceException("trigger == null");
+            }
+            if (branch == null)
+            {
+                throw new NullReferenceException("branch == null");
+            }
+
+            Binding binding = new Binding(trigger, branch);
+
+            bindings.Add(binding);
+        }
+
+        public void Unbind(Node trigger)
+        {
+            bindings.RemoveAll(binding
+                => ReferenceEquals(binding.Trigger, trigger));
+        }
+
+        private class Binding
+        {
+            public Node Trigger { get { return trigger; } }
+
+            private readonly Node trigger;
+            private readonly Node branch;
+            private Status previousStatus;
+
+            public Binding(Node trigger, Node branch)
+            {
+                this.trigger = trigger;
+                this.branch = branch;
+                previousStatus = Status.Unknown;
+            }
+
+            public void Reset()
+            {
+                branch.Terminate();
+                previousStatus = Status.Unknown;
+            }
+
+            public void Step()
+            {
+                Status currentStatus = trigger.Status;
+
+                if (previousStatus != currentStatus
+                    && currentStatus == Status.Success)
+                {
+                    Log.Verbose("Status changed to {0}", currentStatus);
+
+                    branch.Tick();
+                    previousStatus = currentStatus;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Environment/Core/Supervisor.cs
@@ -0,0 +1,43 @@
+﻿using BT;
+using System;
+using System.Collections.Generic;
+
+namespace RobotNursery
+{
+    public class Supervisor
+    {
+        private readonly Node model;
+        private List<Layer> layers = new List<Layer>();
+
+        public Supervisor(Node model)
+        {
+            if (model == null)
+            {
+                throw new NullReferenceException("model == null");
+            }
+
+            this.model = model;
+        }
+
+        public void Reset()
+        {
+            model.Terminate();
+
+            layers.ForEach(layer => layer.Reset());
+        }
+
+        public void Step()
+        {
+            model.Tick();
+
+            layers.ForEach(layer => layer.Step());
+        }
+
+        public Layer CreateLayer()
+        {
+            Layer layer = new Layer();
+            layers.Add(layer);
+            return layer;
+        }
+    }
+}
