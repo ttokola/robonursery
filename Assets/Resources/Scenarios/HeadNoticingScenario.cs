37e0f70fe6ce747190cf683c312e1247d5b2bc47
mimartinviita <17248982+mimartinviita@users.noreply.github.com>
2018-01-07 19:20:16 +0200

    Dynamic scenario initilization

---
+++ Assets/Resources/Scenarios/HeadNoticingScenario.cs
@@ -0,0 +1,34 @@
+﻿using BT;
+using UnityEngine;
+
+namespace Env
+{
+    public class HeadNoticingScenario : Scenario
+    {
+        public GameObject head;
+
+        public override Node Create()
+        {
+            // Prepare environment actions
+
+            Action spawnTargetAction = new SpawnBallAction(
+                "TargetBall", new Vector3(4.14f, 1.38f, 2.00f));
+            Action destroyTargetAction = new DestroyObjectAction(
+                "TargetBall");
+
+            // Prepare agent objectives
+
+            Objective noticeTargetObjective =
+                new NoticeTargetObjective(head, "TargetBall");
+
+            // Construct scenario
+
+            Sequence sequence = new Sequence();
+            sequence.Add(spawnTargetAction);
+            sequence.Add(noticeTargetObjective);
+            sequence.Add(destroyTargetAction);
+
+            return sequence;
+        }
+    }
+}
--- Assets/Scenarios/TestController.cs
+++ Assets/Scenarios/TestController.cs
@@ -5,10 +5,16 @@ using Env;
 public class TestController : MonoBehaviour
 {
     private Sequence schedule;
+    private System.Collections.Generic.List<string> scenarioslist = new System.Collections.Generic.List<string>();
+
 
     void Start()
     {
+        DontDestroyOnLoad(this.transform.gameObject);
+
         schedule = new Sequence();
+
+        Initialize();
 
         var scenarios = transform.Find("Scenarios").GetComponents<Scenario>();
 
@@ -18,6 +24,27 @@ public class TestController : MonoBehaviour
             schedule.Add(new DebugAction(scenario.GetType().Name + " start"));
             schedule.Add(node);
             schedule.Add(new DebugAction(scenario.GetType().Name + " end"));
+        }
+    }
+
+    void Initialize()
+    {
+        Object[] objects = Resources.LoadAll("Scenarios");
+
+        foreach (Object obj in objects)
+        {
+            Debug.Log(obj.name);
+            scenarioslist.Add(obj.name);
+        }
+        Resources.UnloadUnusedAssets();
+
+        int num = scenarioslist.Count;
+        Debug.Log("Number of Scenario files: " + num);
+
+        var list = scenarioslist.ToArray();
+        foreach (var item in list)
+        {
+            transform.Find("Scenarios").gameObject.AddComponent(System.Type.GetType(item));
         }
     }
 
